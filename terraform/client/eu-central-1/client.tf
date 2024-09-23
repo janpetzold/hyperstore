6890b97cc51e5477079b15ab3f7be1af599d4163
Jan Petzold <janpetzold@outlook.com>
2024-09-23 21:56:01 +0200

    Outcommented SSH for now

--- terraform/client/eu-central-1/client.tf
+++ terraform/client/eu-central-1/client.tf
@@ -51,30 +51,30 @@ resource "aws_route_table_association" "main" {
 }
 
 # Security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "SSH from anywhere"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow_ssh"
-  }
-}
+#   tags = {
+#     Name = "allow_ssh"
+#   }
+# }
 
 
 # Client VM
@@ -84,7 +84,7 @@ resource "aws_instance" "locust-client" {
     instance_type = "t2.nano"
     subnet_id     = aws_subnet.main.id
     iam_instance_profile = aws_iam_instance_profile.instance.name
-    vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+    #vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
     associate_public_ip_address = true
     tags = {
       Name = "Locust EU Client"
